6181d0674835d974cbdbcca96649fd3bf6948423
bogda <bogdanbasinskij371@gmail.com>
2026-05-28 10:15:03 +0000

    lab 5

---
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+use App\Models\BlogCategory;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class CategoryController extends BaseController
+{
+    public function index()
+    {
+        $paginator = BlogCategory::paginate(5);
+
+        return $paginator;
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        if (empty($data['slug']) && !empty($data['title'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+
+        $item = BlogCategory::create($data);
+
+        if ($item) {
+            return ['success' => 'Успішно збережено', 'id' => $item->id];
+        } else {
+            return ['msg' => 'Помилка збереження'];
+        }
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $item = BlogCategory::find($id);
+
+        if (empty($item)) {
+            return response()->json(['errors' => ['msg' => "Запис id=[{$id}] не знайдено"]], 404);
+        }
+
+        $data = $request->all();
+        if (empty($data['slug']) && !empty($data['title'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+
+        $result = $item->update($data);
+
+        if ($result) {
+            return ['success' => 'Успішно збережено'];
+        } else {
+            return ['msg' => 'Помилка збереження'];
+        }
+
+    }
+    public function destroy($id)
+    {
+        $item = BlogCategory::find($id);
+
+        if (empty($item)) {
+            return response()->json(['errors' => ['msg' => "Запис id=[{$id}] не знайдено"]], 404);
+        }
+
+        $result = $item->delete();
+
+        if ($result) {
+            return ['success' => "Запис з id {$id} успішно видалено!"];
+        } else {
+            return response()->json(['errors' => ['msg' => 'Помилка видалення']], 500);
+        }
+    }
+
+    public function show(string $id)
+    {
+        $item = BlogCategory::find($id);
+
+        if (empty($item)) {
+            return response()->json(['errors' => ['msg' => "Запис id=[{$id}] не знайдено"]], 404);
+        }
+
+        return $item;
+    }
+}
