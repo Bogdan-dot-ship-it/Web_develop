bd10a4572c6c7e7b1e182e63e3ca6d7d99713a16
bogda <bogdanbasinskij371@gmail.com>
2026-06-03 20:09:29 +0000

    lab 12

--- app/Http/Controllers/Blog/Admin/PostController.php
+++ app/Http/Controllers/Blog/Admin/PostController.php
@@ -8,6 +8,8 @@ use App\Http\Requests\BlogPostUpdateRequest;
 use Illuminate\Support\Str;
 use App\Models\BlogPost;
 use App\Http\Requests\BlogPostCreateRequest;
+use App\Jobs\BlogPostAfterCreateJob;
+use App\Jobs\BlogPostAfterDeleteJob;
 class PostController extends BaseController
 {
     public function __construct(
@@ -47,11 +49,14 @@ class PostController extends BaseController
 
     public function store(BlogPostCreateRequest $request)
     {
-        $data = $request->input(); // отримаємо масив даних, які надійшли з форми
+        $data = $request->input();
 
-        $item = (new BlogPost())->create($data); // створюємо об'єкт і додаємо в БД
+        $item = (new BlogPost())->create($data);
 
         if ($item) {
+            $job = new BlogPostAfterCreateJob($item);
+            dispatch($job);
+
             return response()->json(['success' => 'Успішно збережено']);
         } else {
             return response()->json(['msg' => 'Помилка збереження'], 500);
@@ -63,6 +68,8 @@ class PostController extends BaseController
         $result = BlogPost::destroy($id);
 
         if ($result) {
+            BlogPostAfterDeleteJob::dispatch($id)->delay(20);
+
             return response()->json(['success' => "Статтю з id {$id} успішно видалено"]);
         } else {
             return response()->json(['msg' => 'Помилка видалення. Можливо, запис не знайдено'], 404);
